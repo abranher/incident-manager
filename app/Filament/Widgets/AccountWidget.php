cb305874bc8481df3950765e82e489d3fdc04cd7
Abraham Hernandez <abrahamycaldera@gmail.com>
2026-02-02 02:26:15 -0400

    refactor: add extended AccountWidget widget from AccountWidget Filament Widget.
    - Delete widgets method from AppPanelProvider.

---
+++ app/Filament/Widgets/AccountWidget.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\AccountWidget as FilamentAccountWidget;
+
+class AccountWidget extends FilamentAccountWidget
+{
+  protected int|string|array $columnSpan = 'full';
+}
+
